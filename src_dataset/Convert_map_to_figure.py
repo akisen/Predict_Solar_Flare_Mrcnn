ef95358b390053ef0785aab3d0729d23a3883f13
Akito Komatsu <akitrb001@gmail.com>
2020-09-23 11:51:45 +0900

    Fitsファイルから回転した画像を書き出すスクリプト修正。

--- src_dataset/Convert_map_to_figure.py
+++ src_dataset/Convert_map_to_figure.py
@@ -2,13 +2,26 @@ from glob import glob
 import sunpy.map
 import cv2
 import astropy.units as u
-fits_path = "/media/akito/Data/HMI_REGION/2010/*201005*/*"
-paths = sorted(glob(fits_path))
+import sys
+import argparse
+from tqdm import tqdm
+import warnings
+warnings.simplefilter('ignore')
+parser = argparse.ArgumentParser()
+parser.add_argument("input_path")
+parser.add_argument("output_path")
+parser.add_argument("--format",default="datetime")
+
+args = parser.parse_args()
+input_path = args.input_path
+paths = sorted(glob(input_path))
 # print(paths)
-for i, path in enumerate(paths):
-    # filename="/home/akito/Documents/Documents/Predict_Solar_Flare_Mrcnn/Dataset/Sun/Image/"+path.split(".")[2][0:15]+".jpg"
-    filename="/home/akito/Documents/Documents/Predict_Solar_Flare_Mrcnn/Dataset/Sun/Image/forffmpeg/"+str(i).zfill(3)+".jpg"
+for i, path in enumerate(tqdm(paths)):
+    if(args.format == "number"):
+        filename=str(args.output_path)+str(i).zfill(3)+".jpg"
+    else:
+        filename=str(args.output_path)+path.split(".")[2][0:15]+".jpg"
     map=sunpy.map.Map(path)
     map_rotated = map.rotate(angle = -1*float(map.meta["crota2"])*u.deg)
-    print(filename)
+    # print(filename)
     cv2.imwrite(filename,map_rotated.data)
