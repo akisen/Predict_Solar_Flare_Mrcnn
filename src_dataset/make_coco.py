df375576688e95cdc7e8a9b057a74e70ebfa0a1e
Akito Komatsu <akitrb001@gmail.com>
2020-09-25 18:38:24 +0900

    AnnotationPickle入出力まで。この先はPickle完成してから

--- src_dataset/make_coco.py
+++ src_dataset/make_coco.py
@@ -3,6 +3,8 @@ import collections as cl
 import argparse
 import sunpy.map
 import glob
+import pickle
+import pandas as pd
 def info():
     tmp = cl.OrderedDict() # OrderedDict:値を追加した順序を記憶することができる辞書型風のデータ構造を提供
     tmp["descripion"] = "Predicting_Solar_Flare"
@@ -14,6 +16,7 @@ def info():
 
 def images(paths):
     paths = glob.glob(paths)
+    tmps =[]
     for path in paths:
         tmp = cl.OrderedDict()
         map = sunpy.map.Map(path)
@@ -24,14 +27,22 @@ def images(paths):
         tmp["width"] = 4096
         tmp["height"] =4096
         tmp["date_captured"] = map.meta['t_rec'][:-4]
-    return tmp
-def annotations():
-    pass
+        tmps.append(tmp)
+    return tmps
+def annotations(pickle_path):
+    tmps = []
+    print(pickle_path)
+    coord_series = pd.read_pickle(pickle_path)
+    print(coord_series.keys())
+    exit()
+    return tmps
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("path",help="input file path.")
+    parser.add_argument("pickle_path")
     args = parser.parse_args()
     paths = args.path
+    pickle_path = args.pickle_path
     query_list = ["info","images","annotations"]
     js = cl.OrderedDict()
     for i in range (len(query_list)):
@@ -41,7 +52,7 @@ def main():
         elif query_list[i] == "images":
             tmp = images(paths)
         else:
-            # tmp = annotations()
+            tmp = annotations(pickle_path)
             pass
         js[query_list[i]] = tmp
     fw = open("datasets.json","w")
